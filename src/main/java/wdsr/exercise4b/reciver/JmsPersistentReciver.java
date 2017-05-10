f1b708ae54c1629edd276ac0426460e857942a51
radekkania <radekkania7@wp.pl>
2017-05-10 20:21:06 +0200

    edit JmsPersistentReciver

--- src/main/java/wdsr/exercise4b/reciver/JmsPersistentReciver.java
+++ src/main/java/wdsr/exercise4b/reciver/JmsPersistentReciver.java
@@ -1,5 +1,6 @@
 package wdsr.exercise4b.reciver;
 
+import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
 import javax.jms.Queue;
+import javax.jms.QueueBrowser;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
@@ -28,10 +30,13 @@ public class JmsPersistentReciver {
 	private Session session;
 	private Queue queue;
 	private MessageConsumer consumer;
+	private QueueBrowser browser;
 	
 	private final int mode = Session.AUTO_ACKNOWLEDGE;
 	private final boolean transacted = false;
 	private final String queueName = "RADEKKANIA.QUEUE";
+	
+	
 
 	public JmsPersistentReciver() {
 		this.factory = new ActiveMQConnectionFactory("tcp://localhost:61616");
@@ -40,37 +45,35 @@ public class JmsPersistentReciver {
 			this.session = connection.createSession(transacted, mode);
 			this.queue = session.createQueue(queueName);
 			this.consumer = session.createConsumer(queue);
-			consumer.setMessageListener(new MessageListener() {
-
-				@Override
-				public void onMessage(Message message) {
-					try {
-						TextMessage mssg = (TextMessage) message;
-						list.add(mssg);
-						log.info("Recive message: " + mssg.getText());
-						System.out.println(mssg.getText());
-					} catch (JMSException e) {
-						log.error("fail on method onMessage", e);
-					}
+			/*consumer.setMessageListener((message) -> {
+				try {
+					TextMessage mssg = (TextMessage) message;
+					list.add(mssg);
+					log.info("Recive message: " + mssg.getText());
+				} catch (JMSException e) {
+					log.error("fail on method onMessage", e);
 				}
-				
-			});
+			});*/
 		} catch (JMSException e) {
-			log.error("failed", e);
+			log.info("fail reciver");
 		}
 	}
+	
 	
 	public void recive() {
 		try {
 			connection.start();
+			TextMessage mssg = (TextMessage) consumer.receive(100);
+			while (mssg != null) {
+				list.add(mssg);
+				log.info("recived " + mssg.getText());
+				mssg = (TextMessage) consumer.receive(100);
+			}
+			log.info("Recvied: + " + list.size() + " messages");
+			shutDown();
 		} catch (JMSException e) {
 			log.error("failed");
-		} finally {
-			waitMilliSeconds(1000L);
-			log.info("Recived " + list.size() + " messages");
-			shutDown();
-			System.exit(0);
-		}
+		} 
 	}
 		
 	private void shutDown() {
@@ -82,13 +85,4 @@ public class JmsPersistentReciver {
 			log.error("cant close session or connection", e);
 		}
 	}
-	
-	private void waitMilliSeconds(Long millis) {
-		try {
-			Thread.sleep(millis);
-		} catch (InterruptedException e) {
-			log.error("waiting failed");
-		}
-	}
-	
 }
